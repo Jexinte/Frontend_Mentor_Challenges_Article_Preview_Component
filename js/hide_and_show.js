033c54681031e8c7b3e8587c4c1248debf2c1c98
Yokke <mdembelepro@gmail.com>
2022-06-24 09:39:16 +0200

    Refactoring code when user clicked on share button

--- js/hide_and_show.js
+++ js/hide_and_show.js
@@ -4,21 +4,13 @@ const arrowDown = document.querySelector('.arrow_down')
 
 
 shareButton.addEventListener('click',() => {
-    if(shareContainer.style.visibility && arrowDown.style.visibility == "hidden") {
-
-        shareContainer.style.visibility="visible"
-        arrowDown.style.visibility="visible"
-        shareButton.style.color ="#F7FCFF"
-        shareButton.style.background = "#6F839B"
-        
-    }
-
-    else {
-
-        shareContainer.style.visibility="hidden"
-        arrowDown.style.visibility="hidden"
-        shareButton.style.color="#718095"
-        shareButton.style.background ="hsl(210, 46%, 95%)"
-    }
-    
+    if (shareContainer.classList.contains("hidden")) {
+        shareContainer.classList.remove("hidden");
+        shareButton.style.color = "#F7FCFF";
+        shareButton.style.background = "#6F839B";
+      } else {
+        shareContainer.classList.add("hidden");
+        shareButton.style.color = "#718095";
+        shareButton.style.background = "hsl(210, 46%, 95%)";
+      }
 })
